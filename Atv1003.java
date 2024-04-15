f359451ea4cd557249edfe7e056e22b13346ae92
Samuca02 <160745767+Samuca02@users.noreply.github.com>
2024-04-14 22:43:22 -0300

    Add files via upload

---
+++ Atv1003.java
@@ -0,0 +1,16 @@
+public class Atv1003 {
+        public static void main(String[] args) {
+            int soma = 0;
+    
+            System.out.println("Números pares de 1 a 100:");
+    
+            for (int i = 1; i <= 100; i++) {
+                if (i % 2 == 0) {
+                    soma += i;
+                    System.out.println(i);
+                }
+            }
+    
+            System.out.println("Soma dos números pares de 1 a 100: " + soma);
+        }
+    }
